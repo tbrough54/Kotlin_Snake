93e2c9f57fc80ad191aba9c4110bfac8da6be379
thomasbrough <tbrough54@gmail.com>
2023-05-18 22:31:55 +0100

    Added in type to stop chance of null pointer

--- src/main/kotlin/GameIcons.kt
+++ src/main/kotlin/GameIcons.kt
@@ -4,7 +4,7 @@ import javax.swing.ImageIcon
 data class GameIcons(val bodyFileName: String = "dot.png",
                      val headFileName: String = "head.png",
                      val appleFileName: String = "apple.png") {
-    var body = ImageIcon("src/main/resources/$bodyFileName").image
-    var head = ImageIcon("src/main/resources/$headFileName").image
-    var apple = ImageIcon("src/main/resources/$appleFileName").image
+    var body: Image = ImageIcon("src/main/resources/$bodyFileName").image
+    var head: Image = ImageIcon("src/main/resources/$headFileName").image
+    var apple: Image = ImageIcon("src/main/resources/$appleFileName").image
 }
