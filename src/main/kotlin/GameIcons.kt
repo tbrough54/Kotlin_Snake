26daac5ed8b91dcb81dc3d20c0820016e3eedd02
thomasbrough <tbrough54@gmail.com>
2023-05-18 22:27:21 +0100

    Changed to correct path

--- src/main/kotlin/GameIcons.kt
+++ src/main/kotlin/GameIcons.kt
@@ -4,7 +4,7 @@ import javax.swing.ImageIcon
 data class GameIcons(val bodyFileName: String = "dot.png",
                      val headFileName: String = "head.png",
                      val appleFileName: String = "apple.png") {
-    var body = ImageIcon("/Users/thomasbrough/IdeaProjects/untitled/src/main/resources/$bodyFileName").image
-    var head = ImageIcon("/Users/thomasbrough/IdeaProjects/untitled/src/main/resources/$headFileName").image
-    var apple = ImageIcon("/Users/thomasbrough/IdeaProjects/untitled/src/main/resources/$appleFileName").image
+    var body = ImageIcon("/Users/thomasbrough/GitRepos/Kotlin_Snake/src/main/resources/$bodyFileName").image
+    var head = ImageIcon("/Users/thomasbrough/GitRepos/Kotlin_Snake/src/main/resources/$headFileName").image
+    var apple = ImageIcon("/Users/thomasbrough/GitRepos/Kotlin_Snake/src/main/resources/$appleFileName").image
 }
